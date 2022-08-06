6c35a7679ce4b696c2f01dc65bb06375d303fe95
carol <carol.caillaud@gmail.com>
2022-08-06 15:58:19 -0200

    asdad

--- date_format.py
+++ date_format.py
@@ -4,21 +4,4 @@
 
 #1st case
 def fd(x):
-	return '12/11/1990'
-print (fd('12111990'))
-
-#1st and 2nd case
-def fd(x):
-	if	x == '12111990':
-		return '12/11/1990'
-	elif x == '13072010':
-		return '13/07/2010'
-
-#1st, 2nd and 3rd case
-def fd(x):
-	if	x == '12111990':
-		return '12/11/1990'
-	elif x == '13072010':
-		return '13/07/2010'
-	elif x == '20012020':
-		return '20/01/2020'
+	
